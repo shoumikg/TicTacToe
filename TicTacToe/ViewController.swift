80b70e40684098324faf3f2381235ad13517fe12
Shoumik Ghosal <shoumikghosal@airasia.com>
2023-01-04 11:17:46 +0530

    Detecting number of terms played

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -18,14 +18,33 @@ class ViewController: UIViewController {
   @IBOutlet weak var cell20: UIButton!
   @IBOutlet weak var cell21: UIButton!
   @IBOutlet weak var cell22: UIButton!
+  @IBOutlet weak var titleLabel: UILabel! {
+    didSet {
+      titleLabel.text = "TicTacToe Game"
+      titleLabel.font = .boldSystemFont(ofSize: 20)
+    }
+  }
+  @IBOutlet weak var playerLabel: UILabel! {
+    didSet {
+      playerLabel.text = "Player One's turn to play"
+    }
+  }
   
   private var playerOneTurn : Bool = true
+  private var occupiedCells : Dictionary<Int, Int> = [:]
   
   @IBAction func cellClicked(_ sender: UIButton) {
-    print("Button clicked!")
-    sender.setTitle(playerOneTurn ? "X" : "O", for: .normal)
-    playerOneTurn.toggle()
-    sender.setTitleColor(.white, for: .normal)
+    if sender.title(for: .normal) == nil {
+      sender.setTitle(playerOneTurn ? "X" : "O", for: .normal)
+      playerOneTurn.toggle()
+      occupiedCells[sender.tag] = playerOneTurn ? 1 : 2
+      sender.setTitleColor(.white, for: .normal)
+      sender.setTitleColor(.white, for: .highlighted)
+      playerLabel.text = playerOneTurn ? "Player One's turn to play" : "Player Two's turn to play"
+    }
+    if occupiedCells.count == 9 {
+      playerLabel.text = "Game over!"
+    }
   }
 }
 
